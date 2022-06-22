c3ab2587147a26c10dcf41768ada5b2bb6842d2d
Kuherrn <kuherrn.gmail.com>
2022-06-22 19:18:14 -0400

    Add project files.

---
+++ COMP2084_MidTerm_NET5/Models/Candidate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace COMP2084_MidTerm_NET5.Models
+{
+    public class Candidate
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [ForeignKey("Job")]
+        public int? JobId { get; set; }
+        public Job Job { get; set; }
+
+    }
+}
